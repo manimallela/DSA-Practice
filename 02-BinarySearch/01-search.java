7ce2290c9b99f99ec51d8897e520152d81805571
Mani <manimallela2328@gmail.com>
2025-11-04 17:35:37 +0530

    Solved Binary search problems

---
+++ 02-BinarySearch/01-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0, high = n-1;
+        return binarySearch(nums, low, high, target);
+    }
+    int binarySearch(int[] nums, int low, int high, int target){
+        if (low > high) return -1;
+        int mid = (low + high) /2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return binarySearch(nums, low, mid - 1, target);
+        } else {
+            return binarySearch(nums, mid + 1, high, target);
+        }
+    }
+}
